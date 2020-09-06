61945c5a987bc984872a76687dcf672911d3e10e
Waylon Carrier Nunes <wayloncn@gmail.com>
2020-09-06 16:38:04 -0400

    implemented a rudamentary spinner for the mobile layout

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -1,3 +1,16 @@
 "use strict";
-console.log('Hello World!');
+// if (window.screen.width < 400) {
+const testimonialPicker = document.querySelector('#testimonialPicker');
+const testimonials = document.querySelector('#testimonials');
+console.log(testimonialPicker.children);
+for (const testimonialSelection of testimonialPicker.children) {
+    const testimonialIndex = +testimonialSelection.id - 1;
+    const testimonialXPosition = window.screen.width * testimonialIndex;
+    testimonialSelection.addEventListener('click', () => {
+        // modify list position to display selected testimonial
+        testimonials.style.left = `-${testimonialXPosition}px`;
+        console.log(testimonials.style.left);
+    });
+}
+// }
 //# sourceMappingURL=main.js.map
--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,1 +1,21 @@
-console.log('Hello World!');
+// if (window.screen.width < 400) {
+const testimonialPicker = document.querySelector(
+  '#testimonialPicker'
+) as HTMLUListElement;
+const testimonials = document.querySelector(
+  '#testimonials'
+) as HTMLUListElement;
+
+console.log(testimonialPicker.children);
+
+for (const testimonialSelection of testimonialPicker.children) {
+  const testimonialIndex = +testimonialSelection.id - 1;
+  const testimonialXPosition = window.screen.width * testimonialIndex;
+
+  testimonialSelection.addEventListener('click', () => {
+    // modify list position to display selected testimonial
+    testimonials.style.left = `-${testimonialXPosition}px`;
+    console.log(testimonials.style.left);
+  });
+}
+// }
