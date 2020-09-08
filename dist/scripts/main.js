83df7ebfa0bc2831e164abd7ce9f26a9a9bbc22b
Waylon Carrier Nunes <wayloncn@gmail.com>
2020-09-08 19:53:51 -0400

    changed implementation for testimonial spinner; used an overflow of scroll and hid the scrollbar instead.

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -1,13 +1,3 @@
 "use strict";
-const testimonialPicker = document.querySelector('#testimonialPicker');
-const testimonials = document.querySelector('#testimonials');
-console.log(testimonialPicker.children);
-for (const testimonialSelection of testimonialPicker.children) {
-    const testimonialIndex = +testimonialSelection.id - 1;
-    const testimonialXPosition = window.screen.width * testimonialIndex;
-    testimonialSelection.addEventListener('click', () => {
-        // modify list position to display selected testimonial
-        testimonials.style.left = `-${testimonialXPosition}px`;
-    });
-}
+const testimonialsEl = document.querySelector('#testimonials');
 //# sourceMappingURL=main.js.map
--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,18 +1,3 @@
-const testimonialPicker = document.querySelector(
-  '#testimonialPicker'
-) as HTMLUListElement;
-const testimonials = document.querySelector(
+const testimonialsEl = document.querySelector(
   '#testimonials'
 ) as HTMLUListElement;
-
-console.log(testimonialPicker.children);
-
-for (const testimonialSelection of testimonialPicker.children) {
-  const testimonialIndex = +testimonialSelection.id - 1;
-  const testimonialXPosition = window.screen.width * testimonialIndex;
-
-  testimonialSelection.addEventListener('click', () => {
-    // modify list position to display selected testimonial
-    testimonials.style.left = `-${testimonialXPosition}px`;
-  });
-}
