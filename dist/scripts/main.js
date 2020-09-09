4ce186c11b1de4c3ed46d1eeda7c4302b474b420
Waylon Carrier Nunes <wayloncn@gmail.com>
2020-09-08 21:31:00 -0400

    tried some ideas to update the indicators when testimonies are scrolled.

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -1,3 +1,18 @@
 "use strict";
 const testimonialsEl = document.querySelector('#testimonials');
+const testimonials = testimonialsEl.children;
+const testimonialVisibleWidth = +window
+    .getComputedStyle(testimonialsEl)
+    .width.replace('px', '');
+const testimonialActualWidth = testimonialsEl.scrollWidth;
+console.log(`Testimonials visible width: ${testimonialVisibleWidth}`);
+console.log(`Testimonials actual width: ${testimonialActualWidth}`);
+testimonialsEl.addEventListener('touchend', () => {
+    // console.log(
+    //   Math.floor(testimonialsEl.scrollLeft / (testimonialVisibleWidth / 2))
+    // );
+    console.log(`current scrollLeft: ${testimonialsEl.scrollLeft}`);
+    console.log(`scrollLeft / actualWidth: ${((testimonialsEl.scrollLeft / testimonialActualWidth) * 10) / 3}`);
+    // console.log((testimonialsEl.scrollLeft / (testimonialActualWidth * 2)) * 10);
+});
 //# sourceMappingURL=main.js.map
--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,3 +1,29 @@
 const testimonialsEl = document.querySelector(
   '#testimonials'
 ) as HTMLUListElement;
+
+const testimonials = testimonialsEl.children;
+
+const testimonialVisibleWidth = +window
+  .getComputedStyle(testimonialsEl)
+  .width.replace('px', '');
+
+const testimonialActualWidth = testimonialsEl.scrollWidth;
+
+console.log(`Testimonials visible width: ${testimonialVisibleWidth}`);
+console.log(`Testimonials actual width: ${testimonialActualWidth}`);
+
+testimonialsEl.addEventListener('touchend', () => {
+  // console.log(
+  //   Math.floor(testimonialsEl.scrollLeft / (testimonialVisibleWidth / 2))
+  // );
+  console.log(`current scrollLeft: ${testimonialsEl.scrollLeft}`);
+
+  console.log(
+    `scrollLeft / actualWidth: ${
+      ((testimonialsEl.scrollLeft / testimonialActualWidth) * 10) / 3
+    }`
+  );
+
+  // console.log((testimonialsEl.scrollLeft / (testimonialActualWidth * 2)) * 10);
+});
